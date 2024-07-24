e9a0435d2fba76821f8678fbfc8769062b413295
Savchenko <savchenko.ra@yandex.ru>
2024-07-24 14:22:28 +0300

    Добавил более информатированное логирование и различную обработку ошибок в зависимости от события (неизвестный тип древесины или неподходящий диаметр).

--- src/main/java/ru/uni/exceptions/UnknownWoodTypeException.java
+++ src/main/java/ru/uni/exceptions/UnknownWoodTypeException.java
@@ -1,7 +1,4 @@
 package ru.uni.exceptions;
 
-public class UnknownWoodTypeException extends Exception {
-    public UnknownWoodTypeException(String message) {
-        super(message);
-    }
+public class UnknownWoodTypeException extends RuntimeException {
 }
